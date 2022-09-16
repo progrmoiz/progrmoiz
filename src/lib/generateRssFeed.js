b8ca96d3675987450c49a05dbb152d276a97c258
Moiz Farooq <progrmoiz@gmail.com>
2022-09-16 15:07:41 +0500

    updated

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,13 +8,13 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Moiz Farooq',
+    email: 'progrmoiz@gmail.com',
   }
 
   let feed = new Feed({
     title: author.name,
-    description: 'Your blog description',
+    description: "I'm Moiz, a software designer and entrepreneur based in Karachi City.",
     author,
     id: siteUrl,
     link: siteUrl,
